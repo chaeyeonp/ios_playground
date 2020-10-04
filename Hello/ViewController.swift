3662f7c6084024bf08aba750e31af9bf9f3725b0
chaeyeonp <chaeyeon.park@outlook.com>
2020-10-04 20:02:34 +0900

    Add reset Button

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -24,5 +24,8 @@ class ViewController: UIViewController {
         print(slider.value)
     }
 
+    @IBAction func touchUpResetButton(_ sender: UIButton){
+        print("touch up reset button")
+    }
 }
 
