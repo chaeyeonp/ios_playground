9bea06ceb7c90cd6cb30901f4afc6f6171154589
chaeyeonp <chaeyeon.park@outlook.com>
2020-10-06 18:33:58 +0900

    5. Add Labels presenting information

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! 
 
     override func viewDidLoad() {
         super.viewDidLoad()
